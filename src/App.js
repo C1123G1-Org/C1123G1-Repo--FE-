504687aeab7eaa4c5eba81c72f06f59bd9c08057
hoangle71200 <108779064+hoangle71200@users.noreply.github.com>
2024-05-30 09:33:34 +0700

    053002

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Comp1 from '../src/component/Comp1';
 import ListPig from './component/hanoi/pig/listPig';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
