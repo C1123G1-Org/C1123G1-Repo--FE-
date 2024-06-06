f41e3e9b73a9acd67222cb28a0d6b680e358af1f
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2024-06-06 16:40:48 +0700

    done FE phân trang + xoá

--- src/services/exportCoteService.js
+++ src/services/exportCoteService.js
@@ -1,10 +1,10 @@
 
 import axios from "axios";
-
+import Cookies from "js-cookie";
 export async function findAll(page){
     let res = await axios.get(`http://localhost:8080/api/exportcotes?page=${page}` , {
         headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
+            'Authorization': 'Bearer ' + Cookies.get('user')
         }
     })
     return res;
@@ -13,7 +13,7 @@ export async function findAll(page){
 export async function deleteList(id){
     let res = await axios.delete(`http://localhost:8080/api/exportcotes/${id}` , {
         headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
+            'Authorization': 'Bearer ' + Cookies.get('user')
         }
     })
     return res;
